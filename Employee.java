5d0b2cd68ba46dcf5b537a752654254f32884bcc
SrikanthReddyMallu <126140093+SrikanthReddyMallu@users.noreply.github.com>
2023-03-08 23:15:44 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,48 @@
+package eom.emp;
+
+public class Employee 
+{
+	private int id;
+	private String name;
+	private int age;
+	private String gender;
+	private String designation;
+	
+	
+
+	public Employee(int id, String name, int age, String gender, String designation) 
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.designation = designation;
+	}
+
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public int getAge() {
+//		return age;
+//	}
+//
+//	public String getGender() {
+//		return gender;
+//	}
+//
+//	public String getDesignation() {
+//		return designation;
+//	}
+	@Override
+	public String toString()
+	{
+		return "id="+id+"name="+name+"age="+age+"gender="+gender+"designation="+designation;
+	}
+
+}
